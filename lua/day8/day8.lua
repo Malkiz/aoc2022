ff186668d1d9623464d974d4b33747f826d72bc4
Omri Eyal <omri.eyal@riseup.co.il>
2023-09-02 12:52:03 +0300

    lua day8 part2

--- lua/day8/day8.lua
+++ lua/day8/day8.lua
@@ -69,3 +69,53 @@ end
 count = count + rowLen * 2 + colLen * 2 - 4
 
 print(count)
+
+local function scenicScore(i, j)
+  local height = tonumber(lines[i]:sub(j, j))
+  local countTop = 0
+  local countBottom = 0
+  local countLeft = 0
+  local countRight = 0
+  for k = i - 1, 1, -1 do
+    countTop = countTop + 1
+    local h = tonumber(lines[k]:sub(j, j))
+    if h >= height then
+      break
+    end
+  end
+  for k = i + 1, colLen do
+    countBottom = countBottom + 1
+    local h = tonumber(lines[k]:sub(j, j))
+    if h >= height then
+      break
+    end
+  end
+  for k = j - 1, 1, -1 do
+    countLeft = countLeft + 1
+    local h = tonumber(lines[i]:sub(k, k))
+    if h >= height then
+      break
+    end
+  end
+  for k = j + 1, rowLen do
+    countRight = countRight + 1
+    local h = tonumber(lines[i]:sub(k, k))
+    if h >= height then
+      break
+    end
+  end
+  -- print(i, j, height, countTop, countLeft, countRight, countBottom)
+  return countTop * countBottom * countLeft * countRight
+end
+
+local maxScore = 0
+for i = 2, colLen - 1 do
+  for j = 2, rowLen - 1 do
+    local score = scenicScore(i, j)
+    if score > maxScore then
+      maxScore = score
+    end
+  end
+end
+
+print(maxScore)
