441f14b9919153de2ba7e2de2bbbf98f87cf7a7b
Omri Eyal <omri.eyal@riseup.co.il>
2023-09-02 08:26:37 +0300

    lua day2 part1

---
+++ lua/day2/day2.lua
@@ -0,0 +1,57 @@
+local open = io.open
+
+local function read_file(path)
+  local file = open(path, "rb") -- r read mode and b binary mode
+  if not file then return nil end
+  local content = file:read "*a" -- *a or *all reads the whole file
+  file:close()
+  return content
+end
+
+local fileContent = read_file("lua/day2/input.txt");
+
+if not fileContent then
+  print("File not found")
+  os.exit()
+end
+
+local moveDict = {
+  ["A"] = "Rock",
+  ["B"] = "Paper",
+  ["C"] = "Scissors",
+  ["X"] = "Rock",
+  ["Y"] = "Paper",
+  ["Z"] = "Scissors",
+}
+
+local outcomeDict = {
+  ["RockRock"] = "Draw",
+  ["RockPaper"] = "Win",
+  ["RockScissors"] = "Lose",
+  ["PaperRock"] = "Lose",
+  ["PaperPaper"] = "Draw",
+  ["PaperScissors"] = "Win",
+  ["ScissorsRock"] = "Win",
+  ["ScissorsPaper"] = "Lose",
+  ["ScissorsScissors"] = "Draw",
+}
+
+local scoreDict = {
+  ["Win"] = 6,
+  ["Lose"] = 0,
+  ["Draw"] = 3,
+  ["Rock"] = 1,
+  ["Paper"] = 2,
+  ["Scissors"] = 3,
+}
+
+local totalScore = 0
+for w in fileContent:gmatch("(.-)\n") do
+  local op, me = w:match("(.+) (.+)")
+  local game = moveDict[op] .. moveDict[me]
+  local outcome = outcomeDict[game]
+  local score = scoreDict[outcome] + scoreDict[moveDict[me]]
+  totalScore = totalScore + score
+end
+
+print(totalScore)
